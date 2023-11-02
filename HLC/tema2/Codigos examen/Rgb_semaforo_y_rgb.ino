91a8f0de76808d6035feb8c7134aa4c38d295488
Bydanixx3001 <danimolinamarin@gmail.com>
2023-11-02 16:38:42 +0100

    esofn

--- HLC/tema2/Codigos examen/Rgb_semaforo_y_rgb.ino
+++ HLC/tema2/Codigos examen/Rgb_semaforo_y_rgb.ino
@@ -1,7 +1,7 @@
 /*led rgb de catodo*/
 int pinred = 9; // Definir los pines de conexión del LED RGB
-int pingreen = 10;
-int pinblue = 11;
+int pingreen = 10; // Definir los pines de conexión del LED RGB
+int pinblue = 11; // Definir los pines de conexión del LED RGB
 void setup() 
 {
   pinMode(pinred, OUTPUT); // configurar los pines como salidas
@@ -13,11 +13,15 @@ void loop()
 {
   setColor(255, 0, 0); //encender en color rojo
   delay(1000); //esperar 1 segundo 
-  setColor(255, 255, 0); //encender en color verde
+  setColor(255, 255, 0); //encender en color amarillo
   delay(1000); 
-  setColor(0, 255, 0); //encender en color azul
+  setColor(0, 255, 0); //encender en color verde
   delay(1000);
-}
+  /*setColor(0, 255, 0); //encender en color verde
+  delay(1000); 
+  setColor(0, 0, 255); //encender en color azul
+  delay(1000);*/
+} 
 void setColor(int red, int green, int blue) //funcion para establecer el color del led rgb
 {
   analogWrite(pinred, red);
