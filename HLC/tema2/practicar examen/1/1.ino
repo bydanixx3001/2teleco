308252301a58b5533c26fa7524181871fe476493
Bydanixx3001 <danimolinamarin@gmail.com>
2023-11-05 15:37:01 +0100

    wvkomdvw

---
+++ HLC/tema2/practicar examen/1/1.ino
@@ -0,0 +1,28 @@
+/*Led con dos interruptores*/ 
+int led = 13;
+int PulsadorApagado= 6;
+int PulsadorEncendido = 7;
+
+void setup ()
+{
+  pinMode(PulsadorApagado, INPUT);
+  pinMode(PulsadorEncendido, INPUT);
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+  if (digitalRead(PulsadorEncendido)== LOW)
+  {
+    digitalWrite(led, LOW);
+    Serial.print("El led está apagado");
+    delay(1000);
+  }
+ 
+  if (digitalRead,(PulsadorApagado)==LOW) 
+  {
+    digitalWrite(led,HIGH);
+    Serial.print("El led está encendido");
+    delay(1000);
+  }
+}
